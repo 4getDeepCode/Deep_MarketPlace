c7c4fc990a2605b7961a092eddc6c7a9d6b6a292
Deep Chand <deepc2060@gmail.com>
2025-12-03 22:52:59 +0530

    initialize project structure with base files

---
+++ backend/src/index.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./db/db");
+
+const app = express();
+
+// Middleware (optional, for JSON parsing)
+app.use(express.json());
+
+// Root Route
+app.get("/", (req, res) => {
+    res.send({ message: "Hello All! Welcome To Deep Marketplace From Backend System 👋" });
+});
+
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, async () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+    await connectDB();
+});
