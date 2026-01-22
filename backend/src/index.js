26a7443c24ca2271d5759f391e21f1f144e37651
Deep Chand <deepc2060@gmail.com>
2026-01-22 11:12:15 +0530

    update client base  url  config to env in  backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CLIENT_BASE_URL,
+    origin: process.env.CLIENT_BASE_URL || "http://localhost:5173",
     credentials: true,
   })
 );
