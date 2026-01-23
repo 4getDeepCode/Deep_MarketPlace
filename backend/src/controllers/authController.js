d9a6a21fd09dd50617fc90416f0d6e6393353ccc
Deep Chand <deepc2060@gmail.com>
2026-01-23 16:32:23 +0530

    enhance authentication logic and service layer

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -3,19 +3,33 @@ const UserError = require("../exceptions/UsserError");
 const AuthService = require("../services/AuthService");
 
 class AuthController {
+  // async sentLoginOtp(req, res) {
+  //   try {
+  //     console.log("BODY:", req.body);
+  //     console.log("EMAIL:", req.body?.email);
+  //     const email = req.body.email;
+  //     await AuthService.sendLoginOtp(email);
+
+  //     return res.status(201).json({ message: "otp sent" });
+  //   } catch (error) {
+  //     if (error instanceof UserError || error instanceof Error) {
+  //       return res.status(400).json({ error: error.message });
+  //     }
+  //     return res.status(500).json({ error: "Internal Server Error" });
+  //   }
+  // }
+
   async sentLoginOtp(req, res) {
     try {
       console.log("BODY:", req.body);
-      console.log("EMAIL:", req.body?.email);
-      const email = req.body.email;
+
+      const { email } = req.body;
       await AuthService.sendLoginOtp(email);
 
       return res.status(201).json({ message: "otp sent" });
     } catch (error) {
-      if (error instanceof UserError || error instanceof Error) {
-        return res.status(400).json({ error: error.message });
-      }
-      return res.status(500).json({ error: "Internal Server Error" });
+      console.error("OTP ERROR:", error.message);
+      return res.status(400).json({ error: error.message });
     }
   }
 
